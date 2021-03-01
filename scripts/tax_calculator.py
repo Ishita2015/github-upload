b36cefd811a5951ff3872220f38537f580184473
Ishita Nigam <Ishita.Nigam@nuance.com>
2021-03-01 17:41:28 +0530

    removed unwanted files'

--- scripts/tax_calculator.py
+++
@@ -1,7 +0,0 @@
-# take input from user and calculate tax and total amount payable
-
-amt = float(input('Enter your amount:'))
-tax_rate = float(input('Enter tax rate:'))
-tax = amt * tax_rate / 100
-total = amt + tax
-print('Amount payable by you is:' + str(total))
